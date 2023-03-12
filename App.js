b34f94e2deb4eb3fdc41b1f3d1973cef1bb28d1b
Denis Nikolsky <den-apple@ya.ru>
2023-03-12 10:10:03 +0200

    chore: настроить окружение

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>App title</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'red',
+    fontSize: 24,
+  },
+});
--- README.md
+++
@@ -1,1 +0,0 @@
-# react-native-crash-course
